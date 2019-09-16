7ab1aee702a384c3a01305f38ca72928e086056e
pipopotamasu <ec0156hx@gmail.com>
2019-09-16 13:28:09 +0900

    extract methods

--- src/methods.ts
+++ src/methods.ts
@@ -1,3 +1,29 @@
-export default function methods (component: any) {
-  console.log(component)
+import Vue, { VueConstructor, ComponentOptions } from 'vue';
+
+interface TargetConstuctor extends VueConstructor {
+  options: ComponentOptions<Vue>
+}
+
+interface MockMethods {
+  [key: string]: Function
+}
+
+function createMockFunction (): Function {
+  return function (args: any) {
+    return {
+      run: () => console.log('run!', args)
+    }
+  }
+}
+
+export default function methods(component: TargetConstuctor) {
+  const methods = component.options.methods;
+  if (!methods) throw new Error('Not exists method.');
+
+  const mockMethods: MockMethods = {};
+  Object.keys(methods).forEach(key => {
+    mockMethods[key] = createMockFunction();
+  });
+
+  return mockMethods;
 }
