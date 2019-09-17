faa2d45644cd771e09a4a20c78bc3b84ad41fe3c
pipopotamasu <ec0156hx@gmail.com>
2019-09-17 09:03:41 +0900

    improve type

--- src/methods.ts
+++ src/methods.ts
@@ -8,21 +8,21 @@ interface MockMethods {
   [key: string]: Function
 }
 
-function createMockFunction (): Function {
-  return function (args: any) {
+function createMockFunction<args>(): Function {
+  return function (args: args) {
     return {
       run: () => console.log('run!', args)
     }
   }
 }
 
-export default function methods(component: TargetConstuctor) {
+export default function methods<args = any>(component: TargetConstuctor) {
   const methods = component.options.methods;
   if (!methods) throw new Error('Not exists method.');
 
   const mockMethods: MockMethods = {};
   Object.keys(methods).forEach(key => {
-    mockMethods[key] = createMockFunction();
+    mockMethods[key] = createMockFunction<args>();
   });
 
   return mockMethods;
