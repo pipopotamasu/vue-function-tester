f9ff4bf70c65e35892bf5c127090b5083c8ddc33
pipopotamasu <ec0156hx@gmail.com>
2019-09-23 13:05:00 +0900

    Add test (#1)
    * extract methods
    
    * improve type
    
    * add target class
    
    * add target object
    
    * base jest setting
    
    * wip
    
    * resolve module by common js

---
+++ src/SampleComponent.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>test</div>
+</template>
+
+<script lang="ts">
+const Vue = require('vue');
+
+export default Vue.extend({
+  methods: {
+    sayHi () {
+      return 'Hi';
+    }
+  }
+})
+</script>
--- src/methods.ts
+++ src/methods.ts
@@ -1,3 +1,28 @@
-export default function methods (component: any) {
-  console.log(component)
+import Vue, { VueConstructor, ComponentOptions } from 'vue';
+import Target from './target';
+
+interface TargetConstuctor extends VueConstructor {
+  options: ComponentOptions<Vue>
+}
+
+interface MockMethods {
+  [key: string]: Function
+}
+
+function createMockFunction(method: Function) {
+  return function<args = any, returnValue = any>(args: args) {
+    return new Target<args, returnValue>(method as (args: args) => returnValue, args);
+  }
+}
+
+export default function methods(component: VueConstructor) {
+  const methods = (component as TargetConstuctor).options.methods;
+  if (!methods) throw new Error('Not exists method.');
+
+  const mockMethods: MockMethods = {};
+  Object.keys(methods).forEach(key => {
+    mockMethods[key] = createMockFunction(methods[key]);
+  });
+
+  return mockMethods;
 }
