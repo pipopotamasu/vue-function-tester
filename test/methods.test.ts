5cd0340db2c050fa2800fa13190c94d3d121c8ec
pipopotamasu <ec0156hx@gmail.com>
2019-09-29 21:45:37 +0900

    add methods test

--- test/methods.test.ts
+++ test/methods.test.ts
@@ -1,11 +1,30 @@
-import SomeComponent from '@src/SampleComponent.vue';
-import tester from '@src/index';
+import SomeComponent from '@test/SampleComponent.vue';
+import NoMethodComponent from '@test/NoMethodComponent.vue';
+import { methods } from '@src/index';
 
 describe('Methods', () => {
   describe('extraction', () => {
     it('extracts methods', () => {
-      const mockMethods = tester.methods(SomeComponent);
-      expect(typeof mockMethods.sayHi).toBe('function')
+      const { sayHi } = methods(SomeComponent);
+      expect(typeof sayHi).toBe('function');
+      expect(typeof sayHi()).toBe('object');
     });
   });
+
+  describe('no method', () => {
+    it('throws no method error', () => {
+      expect(() => {
+        methods(NoMethodComponent);
+      }).toThrow('Not exists method.');
+    });
+  });
+
+  // describe('mock', () => {
+  //   describe('return', () => {
+  //     it('returns value', () => {
+  //       const { sayHi } = methods(SomeComponent);
+  //       expect(sayHi().return).toBe('Hi');
+  //     });
+  //   });
+  // });
 });
