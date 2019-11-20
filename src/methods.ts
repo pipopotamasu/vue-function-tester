8707686174a98f193a68e24551c368c9aec0ba15
pipopotamasu <ec0156hx@gmail.com>
2019-11-21 00:10:53 +0900

    wip

--- src/methods.ts
+++ src/methods.ts
@@ -1,31 +1,40 @@
 import Result from './result';
 import { createBaseContext } from './uitls/context';
 
+interface MockFunction {
+  (...args: any): any,
+  run: Function,
+  r: Function
+}
 interface MockMethods {
-  [key: string]: Function;
+  [key: string]: MockFunction | ReturnType<typeof jest.fn>;
 }
 
-function createMockFunction(methods: MockMethods, methodName: string) {
-  return function(...args: any) {
-    const run = (injectContext: Record<string, any>) => {
-      Object.keys(methods).forEach((k) => {
-        if (methodName !== k) methods[k] = jest.fn();
-      });
-      const context = Object.assign(
-        {},
-        createBaseContext(),
-        methods,
-        injectContext
-      );
-      const returnVal = methods[methodName].apply(context, args);
+function createMockFunction(methods: { [key: string]: Function }, methodName: string) {
+  const mockMethods = { ...methods } as MockMethods;
+  const createRunner = (args: any[] | null = null) => (injectContext: Record<string, any>) => {
+    Object.keys(methods).forEach((k) => {
+      if (methodName !== k) mockMethods[k] = jest.fn();
+    });
+    const context = Object.assign(
+      {},
+      createBaseContext(),
+      mockMethods,
+      injectContext
+    );
+    const returnVal = methods[methodName].apply(context, args ? args: arguments);
 
-      return new Result(returnVal, context);
-    };
+    return new Result(returnVal, context);
+  }
+
+  const mockFn: MockFunction = (...args: any[]) => {
     return {
-      run,
-      r: run
+      run: createRunner(args),
+      r: createRunner(args)
     };
-  };
+  }
+  mockFn.run = mockFn.r = createRunner();
+  return mockFn;
 }
 
 export default function methods(component: any) {
@@ -38,7 +47,7 @@ export default function methods(component: any) {
 
   if (!methods) throw new Error('Not exists method.');
 
-  const mockMethods: MockMethods = {};
+  const mockMethods: { [key: string]: MockFunction } = {};
   Object.keys(methods).forEach((key) => {
     mockMethods[key] = createMockFunction({ ...methods }, key);
   });
